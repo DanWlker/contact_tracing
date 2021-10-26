fc640f9098b7c292cd131a399efcbd3fda0d0428
DanWlker <danielhee2@gmail.com>
2021-10-26 17:42:35 +0800

    First draft of completed sound ping pong system... asdfhjkl

--- lib/proximitytasks/SoundProximityDetection.dart
+++ lib/proximitytasks/SoundProximityDetection.dart
@@ -24,8 +24,12 @@ class SoundProximityDetection implements ProximityDetection{
     }
 
     this.isRunning = true;
+    StopwatchUtility.instance.stopwatch.start();
     //TODO:broadcast signal
+    SoundPlayer.instance.toggleSignal();
+    sleep(Duration(seconds:1));
     //TODO:start listening for signal's return
+    SoundListener.instance.toggleListener(this.callbackFunctionForBroadcaster);
   }
 
   void listenForSignal(BuildContext context) {
@@ -56,6 +60,7 @@ class SoundProximityDetection implements ProximityDetection{
     }
 
     //TODO:calculate average delay by dividing the total of the average by 3
+    averageDelayMs = averageDelayMs ~/ 3;
   }
 
   void startInternalDelayTest() {
@@ -99,10 +104,24 @@ class SoundProximityDetection implements ProximityDetection{
 
   void callbackFunctionForBroadcaster() {
     //TODO:start calculating the distance between two ppl
+    int totalTime = StopwatchUtility.instance.stopwatch.elapsedMilliseconds;
+    StopwatchUtility.instance.stopwatch.stop();
+    StopwatchUtility.instance.stopwatch.reset();
+    int actualTime = totalTime - averageDelayMs - 6000; //minus the internal delay and predefined time to wait
+    double estimatedDistanceInMetres = actualTime * 0.343; //speed of sound is 343m/s, which is 0.343m/ms
+    print(estimatedDistanceInMetres);
   }
 
   void callbackFunctionForListener() {
     //TODO:wait for a few seconds minus the delay and broadcast the return signal
+    int waitDurationMilliseconds = 6000 - averageDelayMs; //predefined time to wait
+
+    Future.delayed(Duration(milliseconds: waitDurationMilliseconds), () {
+      // Here you can write your code
+      SoundPlayer.instance.toggleSignal();
+      sleep(Duration(seconds: 1));
+      SoundPlayer.instance.toggleSignal();
+    });
   }
 
   void callbackFunctionForInternalDelayTest() {
